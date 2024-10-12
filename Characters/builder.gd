77a3a96442feb486f92f9d92c45080b7ef805788
alligator-forest <anthonyjoseph0908@gmail.com>
2024-10-12 14:28:13 -0400

    Merchants
    Added Merchants

--- Characters/builder.gd
+++ Characters/builder.gd
@@ -1,2 +1,12 @@
 extends Character
 class_name Builder
+
+func getBricks() -> int:
+	return 0
+
+func getGold() -> int:
+	print("A")
+	return 0
+
+func getGodFear() -> int:
+	return 0
---
+++ Characters/merchant.gd
@@ -0,0 +1,9 @@
+extends Character
+class_name Merchant
+
+func getGold() -> int:
+	print("B")
+	return 5
+
+func getGodFear() -> int:
+	return 2
