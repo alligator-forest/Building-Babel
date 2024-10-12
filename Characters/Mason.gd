1c9d105263d856eab57d536d3fe61cd2c4a55a7d
alligator-forest <anthonyjoseph0908@gmail.com>
2024-10-12 14:57:21 -0400

    Mason
    Added the Mason

---
+++ Characters/Mason.gd
@@ -0,0 +1,9 @@
+extends Character
+class_name  Mason
+
+func getBricks() -> int:
+	return 5
+
+func getGodFear() -> int:
+	return 1
+
--- Characters/builder.gd
+++ Characters/builder.gd
@@ -1,12 +1,5 @@
 extends Character
 class_name Builder
 
-func getBricks() -> int:
-	return 0
-
 func getGold() -> int:
-	print("A")
-	return 0
-
-func getGodFear() -> int:
 	return 0
