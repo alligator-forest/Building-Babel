5c22554468827f5fb669553c4df5e2ad69b7ebd8
alligator-forest <anthonyjoseph0908@gmail.com>
2025-01-05 17:49:44 -0500

    SFX Volume Slider
    -Added a working volume slider for the sound effects

--- Screens/sound_manager.gd
+++ Screens/sound_manager.gd
@@ -7,16 +7,17 @@ func play_effect(key : String):
 	key = key.to_lower()
 	match key:
 		"bricks":
-			$Brick.play(55)
+			$SFX/Brick.play(55)
 		"gold":
-			$Gold.play()
+			$SFX/Gold.play()
 		"steal":
-			$Steal.play()
+			$SFX/Steal.play()
 		"new floor":
-			$NewFloor.play(24)
+			$SFX/NewFloor.play(24)
 
 func _on_music_slider_value_changed(value: float) -> void:
 	$BGMusic.volume_db = linear_to_db(value)
 
 func _on_sfx_slider_value_changed(value: float) -> void:
-	sfxVolume = value
+	for s in $SFX.get_children():
+		s.volume_db = linear_to_db(value)
