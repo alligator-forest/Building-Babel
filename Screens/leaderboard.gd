09b354bf0e67e1606fc98b5146fe669d1cb0045c
alligator-forest <anthonyjoseph0908@gmail.com>
2025-01-13 19:40:43 -0500

    More Screen Updates
    -Updated Game Over screen for the previous tower design change
    -Changed some minor bbcode text in leaderboard screen
    -Added a background for leaderboard and credits screens

--- Screens/leaderboard.gd
+++ Screens/leaderboard.gd
@@ -6,9 +6,13 @@ func _ready() -> void:
 	var scores = SAVEOBJECT.saveData.get_scores()
 	for i in range(scores.size()):
 		if(scores[i] > 0):
-			$Scores.text +="\t" + SCOREKEEPER.format_time(scores[i]) + "\n"
+			if(i == 0):
+				$Scores.text += "[rainbow freq=0.2 val=0.85][tornado radius=3 freq=5.0]"
+				$Scores.text +="\t" + SCOREKEEPER.format_time(scores[i]) + "[/tornado][/rainbow]\n"
+			else:
+				$Scores.text +="\t" + SCOREKEEPER.format_time(scores[i]) + "\n"
 		else:
-			$Scores.text += "\tScore Not Set!\n"
+			$Scores.text += "\t--:--:--\n"
 
 
 func _on_button_pressed() -> void:
